a9252e82ba4736792fee07a53fd5100219f67930
Gordon Goodrum <stopgordy@gmail.com>
2018-07-22 13:09:49 -0500

    dynamodb table functions

---
+++ lambda/app/route/database/CreateTable.ts
@@ -0,0 +1,28 @@
+import {AmazonWebService} from "../../../../lib/model/aws/AmazonWebService";
+
+let aws = new AmazonWebService();
+let dynamoDB = new aws.AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+let params = {
+    TableName: 'ENTRY_LIST',
+    StreamSpecification: {
+        StreamEnabled: false
+    },
+    AttributeDefinitions: [
+        {AttributeName: 'ENTRY_ID', AttributeType: 'N'},
+        {AttributeName: 'ENTRY_NAME', AttributeType: 'S'}
+    ],
+    KeySchema: [
+        {AttributeName: 'ENTRY_ID', KeyType: 'HASH'},
+        {AttributeName: 'ENTRY_NAME', KeyType: 'RANGE'}
+    ],
+    ProvisionedThroughput: {ReadCapacityUnits: 1, WriteCapacityUnits: 1}
+};
+
+dynamoDB.createTable(params, function (err, data) {
+    if (err) {
+        console.log("Error", err);
+    } else {
+        console.log("Table Created", data);
+    }
+});
--- lambda/app/route/database/ListTables.ts
+++ lambda/app/route/database/ListTables.ts
@@ -1,16 +1,12 @@
 import {AmazonWebService} from "../../../../lib/model/aws/AmazonWebService";
-import {DynamoDB} from "../../../../lib/model/aws/dynamodb/DynamoDB";
 
 let aws = new AmazonWebService();
-
-// Create the DynamoDB service object
 let dynamoDB = new aws.AWS.DynamoDB({apiVersion: '2012-10-08'});
 
-// Call DynamoDB to retrieve the list of tables
-dynamoDB.listTables({Limit: 10}, function(err, data) {
-	if (err) {
-		console.log("Error", err.code);
-	} else {
-		console.log("Table names are ", data.TableNames);
-	}
+dynamoDB.listTables({Limit: 99}, function (err, data) {
+    if (err) {
+        console.log("Error", err.code);
+    } else {
+        console.log("Response", data.TableNames);
+    }
 });
