a8a801acfbdbc130463d8202dd1fb38f002a6caa
Gordon Goodrum <stopgordy@gmail.com>
2020-03-03 20:36:47 -0600

    MD5 and Utility Functions

---
+++ lib/model/MD5.spec.ts
@@ -0,0 +1,14 @@
+import {expect} from 'chai';
+import {MD5} from "./MD5";
+
+describe('MD5', () => {
+
+    it('should return validated MD5 results', () => {
+
+        expect(MD5('a')).equal('0cc175b9c0f1b6a831c399e269772661');
+        expect(MD5('aaabbb')).equal('6547436690a26a399603a7096e876a2d');
+        expect(MD5('0cc175b9c0f1b6a831c399e269772661')).equal('d7afde3e7059cd0a0fe09eec4b0008cd');
+
+    });
+
+});
