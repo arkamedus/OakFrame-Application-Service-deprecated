acf37e4618879ced8f493df5099fca9bcf73725a
Gordon Goodrum <stopgordy@gmail.com>
2020-04-20 14:59:19 -0500

    Index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+import {ApplicationServer} from "./lib/model/ApplicationServer";
+import {Check} from "./lib/model/Check";
+
+export default {
+ApplicationServer:ApplicationServer,
+    Check: Check
+};
